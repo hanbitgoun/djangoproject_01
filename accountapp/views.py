6d57473ee051af8f343eefe79d1959505d1e9b3b
hanbitgoun <akdntm946@naver.com>
2021-07-08 16:12:28 +0900

    20210708_w commit_4

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
+
 def hello_world(request):
-    # 분기점
+
     if request.method == 'POST':
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'POST METHOD!'})
+                      context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'GET METHOD!'})
+                      context={'text': 'GET METHOD!'})
