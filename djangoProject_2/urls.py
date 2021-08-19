67cd9575dddd725e31b0a9605b84742fda53de60
hanbitgoun <akdntm946@naver.com>
2021-08-19 14:57:50 +0900

    20210819 make subscribe

--- djangoProject_2/urls.py
+++ djangoProject_2/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),  # articleapp
     path('comments/', include('commentapp.urls')),
     path('projects/', include('projectapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # profile image 파일 경로
 
