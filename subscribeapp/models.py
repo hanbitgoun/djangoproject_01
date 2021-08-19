2c08518022b2a4d170d5611d59b8bb4a7ac6c711
hanbitgoun <akdntm946@naver.com>
2021-08-19 15:07:22 +0900

    20210819 make subscribe model

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    # CASCADE - 종속
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+
+    # project 중복 구독 불가
+    class Meta:
+        unique_together = ['user', 'project']
